3c1a2d1bd1267556cad6bdb768fc28a232ea048c
priyanu17 <anu396priya@gmail.com>
2022-11-03 16:38:23 +0530

    fundamentals part 1

---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,36 @@
+console.log('prints to console  ');
+
+let userName = 'anu';
+
+// console.log('Name is : ' + userName);
+
+// let myFirstJob = 'Programmer/ Coder';
+
+// let mySecondJob = 'Cook + Clean  ';
+
+// console.log('My first job is : ' + myFirstJob);
+
+// console.log('My second job is : ' + mySecondJob);
+
+let javascriptIsFun = false;
+
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'YES!!!!';
+
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+console.log(Object);
+console.log(window);
+
+ai = 'hello';
+console.log(window);
+
+console.log(window.ai);

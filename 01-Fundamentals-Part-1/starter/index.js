246d86a8f8ef122ee3c1c3489fc681150c594a04
priyanu17 <anu396priya@gmail.com>
2022-11-04 10:57:34 +0530

    type conversion and coercion

--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -12,25 +12,59 @@ let userName = 'anu';
 
 // console.log('My second job is : ' + mySecondJob);
 
-let javascriptIsFun = false;
+// let javascriptIsFun = false;
 
-console.log(javascriptIsFun);
-console.log(typeof javascriptIsFun);
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 
-javascriptIsFun = 'YES!!!!';
+// javascriptIsFun = 'YES!!!!';
 
-console.log(javascriptIsFun);
-console.log(typeof javascriptIsFun);
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 
-let year;
+// let year;
 
-console.log(year);
-console.log(typeof year);
+// console.log(year);
+// console.log(typeof year);
 
-console.log(Object);
-console.log(window);
+// console.log(Object);
+// console.log(window);
 
-ai = 'hello';
-console.log(window);
+// ai = 'hello';
+// console.log(window);
 
-console.log(window.ai);
+// console.log(window.ai);
+
+let num = 23;
+let str = '23';
+console.log(Number(num));
+console.log(Number(str));
+
+// let str2 = 'sdfsdf';
+
+// console.log(Number(str2));
+
+// console.log(Number('random text'));
+
+// console.log(String(343534545));
+// console.log(String('78'));
+
+console.log(Boolean(0));
+console.log(Boolean('0'));
+console.log(Boolean(''));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean({}));
+
+// only 5 falsy values
+
+// 0, '', null, undefined, NaN
+
+const money = 90;
+
+if (money > 0) {
+  console.log('You have ' + money + ' dollars!');
+} else {
+  console.log('You should get a job!');
+}

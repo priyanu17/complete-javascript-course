0c6d7981c13ef56c1d17538a6045f37a44184381
priyanu17 <anu396priya@gmail.com>
2022-11-07 17:34:21 +0530

    chain promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -58,6 +58,26 @@ const countriesContainer = document.querySelector('.countries');
 
 // console.log(res, 'RES');
 
+const renderCountry = data => {
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${data?.flags?.png || ''}" />
+      <div class="country__data">
+        <h3 class="country__name">${data?.name?.common || ''}</h3>
+        <h4 class="country__region">${data?.region || ''}</h4>
+        <p class="country__row"><span>👫</span>${
+          (+data?.population / 1000000).toFixed(2) + '  people'
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          data?.currencies?.INR?.name || ''
+        }</p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 const getCountryData = () => {
   fetch('https://restcountries.com/v3.1/name/denmark')
     .then(response => {
@@ -66,6 +86,21 @@ const getCountryData = () => {
     })
     .then(response => {
       console.log(response);
+      renderCountry(response[0]);
+      return 23;
+    })
+    .then(data => console.log(data))
+    .then(() => {
+      return fetch('https://restcountries.com/v3.1/name/india');
+    })
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(response => {
+      console.log(response);
+      renderCountry(response[0]);
+      return 23;
     });
 };
 

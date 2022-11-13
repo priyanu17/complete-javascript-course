7e288d0b4f2bdd7d18cfbefe1abc365284cff060
priyanu17 <anu396priya@gmail.com>
2022-11-13 08:51:19 +0530

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,12 +2,28 @@
 
 console.log('hi');
 
-function logger(se) {
-  console.log('Log from logger function');
-}
-
 // logger('sd', 'sdf');
 // const se = logger(34);
 // console.log(se);
 
-// console.log(console.assert('sdf'));
+// function declaration
+
+function calcAge1(birthYear) {
+  const age = 2022 - birthYear;
+  return age;
+}
+
+// function expression
+const calcAge2 = function (birthYear) {
+  const age = 2022 - birthYear;
+  return age;
+};
+
+// arrow function
+const calcAge3 = (birthYear) => 2022 - birthYear;
+
+const age1 = calcAge1(1996);
+const age2 = calcAge2(1996);
+const age3 = calcAge3(1996);
+
+console.log(age1, age2, age3);

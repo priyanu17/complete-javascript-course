44a43330d437262098ce662b90204a11c43de012
priyanu17 <anu396priya@gmail.com>
2022-11-14 12:09:53 +0530

    functions challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -26,4 +26,24 @@ const age1 = calcAge1(1996);
 const age2 = calcAge2(1996);
 const age3 = calcAge3(1996);
 
-console.log(age1, age2, age3);
+// console.log(age1, age2, age3);
+
+// coding challenge
+
+const calAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// console.log(calAvg(22, 21, 23));
+
+const checkWinner = function (s1, s2, s3, b1, b2, b3) {
+  const avg1 = calAvg(s1, s2, s3);
+  const avg2 = calAvg(b1, b2, b3);
+  if (avg1 > 2 * avg2) {
+    console.log('Team 1 wins');
+  } else if (avg2 > 2 * avg1) {
+    console.log('Team 2 wins');
+  } else {
+    console.log('No team wins');
+  }
+};
+
+// checkWinner(89, 74, 23, 45, 223, 232);
